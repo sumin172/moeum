f9f549b191df2779afb20a150031a613ab8e77fc
Mas <172sumin@gamil.com>
2026-07-19 23:45:09 +0900

    테스트

--- apps/backend/bootstrap/src/main/kotlin/com/moeum/MoeumApplication.kt
+++ apps/backend/bootstrap/src/main/kotlin/com/moeum/MoeumApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 // UserDetailsServiceAutoConfiguration: JWT만으로 인증하고 UserDetailsService/AuthenticationProvider를
-// 쓰지 않으므로, Spring Boot가 기본 생성하는 인메모리 사용자가 불필요해 명시적으로 제외
+// 쓰지 않으므로, Spring Boot가 기본 생성하는 인메모리 사용자가 불필요해 명시적으로 제외 테스트
 @SpringBootApplication(exclude = [UserDetailsServiceAutoConfiguration::class])
 @ConfigurationPropertiesScan
 class MoeumApplication
